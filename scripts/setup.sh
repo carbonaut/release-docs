e71fde3200eae899a7031025822d12b6911df80e
larissa <lari.barabasz@gmail.com>
2019-08-11 21:46:12 -0300

    feat(setup): add "semantic-release" to package scripts.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,3 +3,5 @@ cd $INIT_CWD
 npm i semantic-release --save-dev
 
 cp ./node_modules/semantic-release-wellabe/scripts/docs/.releaserc.json .
+
+json -I -f package.json -e 'this.scripts["semantic-release"]="semantic-release"'
