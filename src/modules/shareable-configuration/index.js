a3d6d41c396e5ec2621cc45c2ae6eb8e33c004f2
larissa <lari.barabasz@gmail.com>
2019-08-17 01:09:12 -0300

    feat(shareable-configuration): update config file.

--- src/modules/shareable-configuration/index.js
+++ src/modules/shareable-configuration/index.js
@@ -2,10 +2,13 @@ module.exports = {
   "plugins": [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
     "@semantic-release/changelog",
+    ["@semantic-release/exec", {
+      "prepareCmd": "release-docs-adjust-version --version=${nextRelease.version}",
+      "successCmd": "release-docs-update-changelog-project"
+    }],
     ["@semantic-release/git", {
-      "assets": ["package-lock.json", "package.json", "CHANGELOG.md"],
+      "assets": ["package-lock.json", "package.json", "CHANGELOG.md", "config.xml"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     "@semantic-release/github"
